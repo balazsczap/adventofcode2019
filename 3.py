5d86133ec203325ac199f835e8908628cf6d50d3
balazsczap <balazs.czap@skyscanner.net>
2019-12-04 23:39:36 +0100

    try2

--- 3.py
+++ 3.py
@@ -45,23 +45,44 @@ def task1_on_input(input):
             progressing_positions = interpolate(last_position, current_position, length)
             all_positions = all_positions  + progressing_positions[1:]
         return all_positions
+            
+    def find_intersections(steps1, line2):
+        all_positions = [(0, 0)]
+        intersections = []
+        current_position = (0, 0)
+        print(line2)
+        for instruction in to_instructions(line2):
+            length = instruction["length"]
+            last_position = current_position
+            current_position = step(current_position, instruction)
+            progressing_positions = interpolate(last_position, current_position, length)
 
+            for pos in progressing_positions:
+                intersections = intersections + filter(lambda step: step[0] == pos[0] and step[1] == pos[1], steps1)
+
+            all_positions = all_positions  + progressing_positions[1:]
+        return [intersections, all_positions]
         
     steps1 = do_all_steps(line1)
-    steps2 = do_all_steps(line2)
 
-    def find_same_steps(step, othersteps):
-        return list(filter(lambda item: item[0] == step[0] and item[1] == step[1], othersteps))
-
-
-
-    matching_steps = filter(lambda x: len(x[1]) > 0, map(lambda step: (step, find_same_steps(step, steps2)), steps1))
-
-    total_distances = map(lambda matches: matches[0][0] + matches[0][1], matching_steps)
-    total_distances_except_zero = filter(lambda x: x>0, total_distances)
-    print(total_distances_except_zero)
-    lowest = min(total_distances_except_zero)
+    [intersections, steps2] = find_intersections(steps1, line2)
+    # print(steps1)
+    print(intersections)
+    lowest = min(filter(lambda x: x>0, map(lambda x: x[0]+x[1], intersections)))
     print(lowest)
+    # print(stesp2)
+    # def find_same_steps(step, othersteps):
+    #     return list(filter(lambda item: item[0] == step[0] and item[1] == step[1], othersteps))
+
+
+
+    # matching_steps = filter(lambda x: len(x[1]) > 0, map(lambda step: (step, find_same_steps(step, steps2)), steps1))
+
+    # total_distances = map(lambda matches: matches[0][0] + matches[0][1], matching_steps)
+    # total_distances_except_zero = filter(lambda x: x>0, total_distances)
+    # print(total_distances_except_zero)
+    # lowest = min(total_distances_except_zero)
+    # print(lowest)
 
 task1_on_input(test1)
 task1_on_input(test2)
